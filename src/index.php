0994303e95c9e151612b8bd51a027e5b39d6188f
Murfidar <murfidar@gmail.com>
2023-11-27 13:46:49 +0700

    chore: changes made on readme.md and src/index.php

--- src/index.php
+++ src/index.php
@@ -6,16 +6,21 @@ Dotenv\Dotenv::createUnsafeImmutable(__DIR__ . '/..' . '')->load();
 
 use BRI\InfoMutasi\InfoMutasi;
 
+// env values
 $clientId = $_ENV['CLIENT_KEY']; // customer key
 $clientSecret = $_ENV['CLIENT_SECRET']; // customer secret
 $pKeyId = $_ENV['PRIVATE_KEY']; // private key
-$account = '234567891012348'; //account
-$startDate = (new DateTime('now', new DateTimeZone('Asia/Jakarta')))->modify('-1 day')->format('Y-m-d\TH:i:sP');; //start date
-$endDate = (new DateTime('now', new DateTimeZone('Asia/Jakarta')))->format('Y-m-d\TH:i:sP'); //end date || current date
-$partnerId = 'feedloop'; //partner id
+
+// url path values
 $baseUrl = 'https://sandbox.partner.api.bri.co.id'; //base url
 $path = '/snap/v1.0/bank-statement'; //informasi mutasi api path
 $accessTokenPath = '/snap/v1.0/access-token/b2b'; //access token path
+
+// change the variables accordingly
+$account = '234567891012348'; //account
+$startDate = (new DateTime('now', new DateTimeZone('Asia/Jakarta')))->modify('-1 day')->format('Y-m-d\TH:i:sP');; //start date
+$endDate = (new DateTime('now', new DateTimeZone('Asia/Jakarta')))->format('Y-m-d\TH:i:sP'); //end date || current date
+$partnerId = ''; //partner id
 
 
 echo "\nResult: " . (new InfoMutasi())->getInfoMutasi(
