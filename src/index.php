0be4af4fe6df27c300c991970b7a7a33b7339f7d
AlfaRiza <malfariza45@gmail.com>
2024-07-03 15:59:05 +0700

    feat: change briapi-sdk path require

--- src/index.php
+++ src/index.php
@@ -1,14 +1,20 @@
 <?php
 
+include 'util.php';
 require __DIR__ . '/../vendor/autoload.php';
 
 Dotenv\Dotenv::createUnsafeImmutable(__DIR__ . '/..' . '')->load();
 
-use BRI\InfoMutasi\InfoMutasi;
+require __DIR__ . '/../../briapi-sdk/autoload.php';
+
+use BRI\Balance\Balance;
+use BRI\Token\AccessToken;
+use BRI\Util\VarNumber;
+use BRI\Signature\Signature;
 
 // env values
-$clientId = $_SERVER['BRI_CLIENT_KEY']; // customer key
-$clientSecret = $_SERVER['BRI_CLIENT_SECRET']; // customer secret
+$clientId = $_ENV['CONSUMER_KEY']; // customer key
+$clientSecret = $_ENV['CONSUMER_SECRET']; // customer secret
 $pKeyId = $_ENV['PRIVATE_KEY']; // private key
 
 // url path values
@@ -17,21 +23,39 @@ $path = '/snap/v1.0/bank-statement'; //informasi mutasi api path
 $accessTokenPath = '/snap/v1.0/access-token/b2b'; //access token path
 
 // change the variables accordingly
-$account = '234567891012348'; //account
+$account = ''; //account
 $startDate = (new DateTime('now', new DateTimeZone('Asia/Jakarta')))->modify('-1 day')->format('Y-m-d\TH:i:sP');; //start date
 $endDate = (new DateTime('now', new DateTimeZone('Asia/Jakarta')))->format('Y-m-d\TH:i:sP'); //end date || current date
 $partnerId = ''; //partner id
+$channelId = ''; // channel id
 
+//external id
+$externalId = (new VarNumber())->generateVar(9);;
 
-echo "\nResult: " . (new InfoMutasi())->getInfoMutasi(
-  $account,
-  $startDate,
-  $endDate,
-  $clientId,
-  $clientSecret,
-  $pKeyId,
-  $partnerId,
-  $baseUrl,
-  $path,
-  $accessTokenPath
-);
+// fetches a new access token every specified minute with a maximum of 15 minutes
+$minutes = 15;
+
+if (!file_exists('accessToken.txt') || isTokenExpired('timestamp.txt', $minutes)) {
+  //timestamp
+  $timestamp = (new DateTime('now', new DateTimeZone('Asia/Jakarta')))->format('Y-m-d\TH:i:s.000P');
+
+  //access token
+  $accessToken = (new AccessToken(new Signature()))->getAccessToken(
+    $clientId,
+    $pKeyId,
+    $timestamp,
+    $baseUrl,
+    $accessTokenPath,
+  );
+
+  file_put_contents('accessToken.txt', $accessToken);
+  file_put_contents('timestamp.txt', $timestamp);
+
+  echo "New Token is created\n";
+} else {
+  $accessToken = trim(file_get_contents('accessToken.txt'));
+  $timestamp = trim(file_get_contents('timestamp.txt'));
+  echo "Used Token\n";
+}
+
+echo (new Balance())->statement($account, $startDate, $endDate, $clientSecret, $partnerId, $baseUrl, $path, $accessToken, $channelId, $externalId, $timestamp);
